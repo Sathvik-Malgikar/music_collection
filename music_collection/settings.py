48bbd46d8778da60cfd05bd941bae6dd071c7e11
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-08-21 14:32:03 +0530

    Update settings.py
    inspired by woring simple test proj

--- music_collection/settings.py
+++ music_collection/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)m3z=(ztt=104f=toiyssyqs&@9@g!skrxiyb0)h-vjrm44c%2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["djangoproject.localhost" , "127.0.0.1","audiolibraryforthetechyfolks.herokuapp.com"]
 
@@ -135,8 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 django_heroku.settings(locals())
 
 # Default primary key field type
